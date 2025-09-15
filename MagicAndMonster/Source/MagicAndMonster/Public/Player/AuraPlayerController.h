4dd4eaf3f50107f5d2a108efd47237653445b47d
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2025-09-15 23:33:16 +0530

    Enhance Input

---
+++ MagicAndMonster/Source/MagicAndMonster/Public/Player/AuraPlayerController.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+UCLASS()
+class MAGICANDMONSTER_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
