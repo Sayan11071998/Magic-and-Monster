0caa546d24f41a6a4cd325dd8f7fa2fe0a7878bd
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2025-09-17 01:19:28 +0530

    Effect Actor

--- MagicAndMonster/Intermediate/Build/Win64/UnrealEditor/Inc/MagicAndMonster/UHT/MagicAndMonster.init.gen.cpp
+++ MagicAndMonster/Intermediate/Build/Win64/UnrealEditor/Inc/MagicAndMonster/UHT/MagicAndMonster.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeMagicAndMonster_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xEF10B57F,
-				0x4B619610,
+				0x3E052A3C,
+				0xD7327F34,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_MagicAndMonster.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_MagicAndMonster.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_MagicAndMonster(Z_Construct_UPackage__Script_MagicAndMonster, TEXT("/Script/MagicAndMonster"), Z_Registration_Info_UPackage__Script_MagicAndMonster, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xEF10B57F, 0x4B619610));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_MagicAndMonster(Z_Construct_UPackage__Script_MagicAndMonster, TEXT("/Script/MagicAndMonster"), Z_Registration_Info_UPackage__Script_MagicAndMonster, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x3E052A3C, 0xD7327F34));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
