ae4d0ebd23bf438685f41027b0ed12774815d03c
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2025-09-18 00:25:16 +0530

    Aura HUD

--- MagicAndMonster/Intermediate/Build/Win64/UnrealEditor/Inc/MagicAndMonster/UHT/MagicAndMonster.init.gen.cpp
+++ MagicAndMonster/Intermediate/Build/Win64/UnrealEditor/Inc/MagicAndMonster/UHT/MagicAndMonster.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeMagicAndMonster_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xB27D1B66,
-				0xB471596A,
+				0x99F6C3FB,
+				0x9DB8F60D,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_MagicAndMonster.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_MagicAndMonster.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_MagicAndMonster(Z_Construct_UPackage__Script_MagicAndMonster, TEXT("/Script/MagicAndMonster"), Z_Registration_Info_UPackage__Script_MagicAndMonster, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xB27D1B66, 0xB471596A));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_MagicAndMonster(Z_Construct_UPackage__Script_MagicAndMonster, TEXT("/Script/MagicAndMonster"), Z_Registration_Info_UPackage__Script_MagicAndMonster, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x99F6C3FB, 0x9DB8F60D));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
