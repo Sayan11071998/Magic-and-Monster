3dcf1d4648f6198f19a4b9eedf5587d4032d8d99
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2025-09-19 00:35:51 +0530

    Callbacks for Mana Change

--- MagicAndMonster/Intermediate/Build/Win64/UnrealEditor/Inc/MagicAndMonster/UHT/MagicAndMonster.init.gen.cpp
+++ MagicAndMonster/Intermediate/Build/Win64/UnrealEditor/Inc/MagicAndMonster/UHT/MagicAndMonster.init.gen.cpp
@@ -8,7 +8,9 @@
 PRAGMA_DISABLE_DEPRECATION_WARNINGS
 void EmptyLinkFunctionForGeneratedCodeMagicAndMonster_init() {}
 	MAGICANDMONSTER_API UFunction* Z_Construct_UDelegateFunction_MagicAndMonster_OnHealthChangedSignature__DelegateSignature();
+	MAGICANDMONSTER_API UFunction* Z_Construct_UDelegateFunction_MagicAndMonster_OnManaChangedSignature__DelegateSignature();
 	MAGICANDMONSTER_API UFunction* Z_Construct_UDelegateFunction_MagicAndMonster_OnMaxHealthChangedSignature__DelegateSignature();
+	MAGICANDMONSTER_API UFunction* Z_Construct_UDelegateFunction_MagicAndMonster_OnMaxManaChangedSignature__DelegateSignature();
 	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_MagicAndMonster;
 	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_MagicAndMonster()
 	{
@@ -16,20 +18,22 @@ void EmptyLinkFunctionForGeneratedCodeMagicAndMonster_init() {}
 		{
 			static UObject* (*const SingletonFuncArray[])() = {
 				(UObject* (*)())Z_Construct_UDelegateFunction_MagicAndMonster_OnHealthChangedSignature__DelegateSignature,
+				(UObject* (*)())Z_Construct_UDelegateFunction_MagicAndMonster_OnManaChangedSignature__DelegateSignature,
 				(UObject* (*)())Z_Construct_UDelegateFunction_MagicAndMonster_OnMaxHealthChangedSignature__DelegateSignature,
+				(UObject* (*)())Z_Construct_UDelegateFunction_MagicAndMonster_OnMaxManaChangedSignature__DelegateSignature,
 			};
 			static const UECodeGen_Private::FPackageParams PackageParams = {
 				"/Script/MagicAndMonster",
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0xF8B8E5E5,
-				0x6A19FD0B,
+				0x99938E98,
+				0x5E347878,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_MagicAndMonster.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_MagicAndMonster.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_MagicAndMonster(Z_Construct_UPackage__Script_MagicAndMonster, TEXT("/Script/MagicAndMonster"), Z_Registration_Info_UPackage__Script_MagicAndMonster, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xF8B8E5E5, 0x6A19FD0B));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_MagicAndMonster(Z_Construct_UPackage__Script_MagicAndMonster, TEXT("/Script/MagicAndMonster"), Z_Registration_Info_UPackage__Script_MagicAndMonster, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x99938E98, 0x5E347878));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
